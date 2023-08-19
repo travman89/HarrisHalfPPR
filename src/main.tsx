9ee698795294c42e6d1ba18b3e99f2b2f6a8ede3
travman89 <travman89@csu.fullerton.edu>
2023-08-19 11:32:44 -0700

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App style={{ backgorundColor: "#f9ca10" }} />
+    <App />
   </React.StrictMode>
 );
