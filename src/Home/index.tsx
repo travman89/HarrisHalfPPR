097115c0e581f148b6871cfe38e1b110ace2835d
travman89 <travman89@csu.fullerton.edu>
2023-11-19 08:17:04 -0800

    11/19 820am update

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -208,7 +208,7 @@ const Home = () => {
         </a>
         . <br /> This site is not affiliated with Christopher Harris.
       </Disclaimer>
-      <Updated>(Updated 11/18 - 8:40am PST)</Updated>
+      <Updated>(Updated 11/19 - 8:20am PST)</Updated>
       <PositionRow>
         <PositionButton
           style={{ border: getUnderline("QB") ? "1px solid" : "none" }}
