4abb75d06411165e11f6514cf43e479811921522
travis davisson <travman89@csu.fullerton.edul>
2024-09-10 18:00:13 -0700

    9/10 6pm update

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -168,7 +168,7 @@ const Home = () => {
   return (
     <Container>
       <FilterContainer>
-        <SubHeading>Week 1 Ranks</SubHeading>
+        <SubHeading>Week 2 Ranks</SubHeading>
         <PositionRow>
           {positions.map((position) => (
             <PositionButton
@@ -260,7 +260,7 @@ const Home = () => {
             </LegendKey>
           )}
         </Legend>
-        <UpdatedText> updated 9/8 - 8am PST</UpdatedText>
+        <UpdatedText> updated 9/10 - 6pm PST</UpdatedText>
       </TableContainer>
       <Heading>Harris Half PPR</Heading>
       <Disclaimer>
