5aa9027b9474646f20bdb8064ea5f8276ebb6f57
travis davisson <travman89@csu.fullerton.edul>
2024-10-20 09:24:21 -0700

    10/20 9:25am  update

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -260,7 +260,7 @@ const Home = () => {
             </LegendKey>
           )}
         </Legend>
-        <UpdatedText> updated 10/20 - 7:10am PST</UpdatedText>
+        <UpdatedText> updated 10/20 - 9:25am PST</UpdatedText>
       </TableContainer>
       <Heading>Harris Half PPR</Heading>
       <Disclaimer>
